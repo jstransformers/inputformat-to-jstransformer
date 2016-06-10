431fff2c9e0dc379b93a46799649277c99d44b6f
Rob Loach <robloach@gmail.com>
2016-06-10 11:55:11 -0400

    Update coding standards

--- build.js
+++ build.js
@@ -5,23 +5,27 @@ var fs = require('fs');
 var sortJson = require('sort-json');
 var npm = require('npm');
 
-var modules = []
+var modules = [];
 for (var i in list) {
   modules.push('jstransformer-' + list[i]);
 }
 
 // Install and load the package.
 npm.load(function (err) {
+  if (err) {
+    throw err;
+  }
+
   // Set up logging.
   npm.on('log', console.log);
 
   npm.commands.install(modules, function (err, data) {
     if (err) {
-      throw new Error(err)
+      throw new Error(err);
     }
-    var dictionary = {}
+    var dictionary = {};
     for (var i in list) {
-      var name = list[i]
+      var name = list[i];
       var transformer = require('jstransformer-' + name);
       var formats = transformer.inputFormats || [name];
 
@@ -38,5 +42,5 @@ npm.load(function (err) {
     }
     var sorted = sortJson(dictionary);
     fs.writeFileSync('dictionary.json', JSON.stringify(sorted, null, 2));
-  })
-})
+  });
+});
